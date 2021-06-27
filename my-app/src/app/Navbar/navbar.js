e684df39d4901c8175ae9c4b1664f9ca65977d87
KT27Learn <66994197+KT27Learn@users.noreply.github.com>
2021-06-27 23:54:52 +0800

    Update Navbar Style V2.1

--- my-app/src/app/Navbar/navbar.js
+++ my-app/src/app/Navbar/navbar.js
@@ -81,24 +81,28 @@ const Navbar = () => {
             <Typography component={link} to="/" className={classes.heading} variant="h2" align="center">Élèven</Typography>
             <img className={classes.image} src={elevenlogo} alt="icon"   height="120" />
           </div>
-          <Link
-            component="button"
-            variant="body2"
-            onClick={() => {
-              history.push('/');
-            }}
-          >
-            Rooms
-          </Link>
-          <Link
-            component="button"
-            variant="body2"
-            onClick={() => {
-              history.push('/library');
-            }}
-          >
-            Library
-          </Link>
+          <div>
+            <Link
+              component="button"
+              variant="body2"
+              onClick={() => {
+                history.push('/');
+              }}
+            >
+              Rooms
+            </Link>
+          </div>
+          <div>
+              <Link
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  history.push('/library');
+                }}
+              >
+                Library
+              </Link>
+          </div>
           <Toolbar className={classes.toolbar}>
             {user ? (
               <div className={classes.profile}>
