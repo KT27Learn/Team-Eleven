7bc8e369bbb5849240f90132158a899ad0c8bbbd
KT27Learn <66994197+KT27Learn@users.noreply.github.com>
2021-06-27 20:30:00 +0800

    Update Index.js with Heroku Backend Url

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://team-eleven-backend.herokuapp.com/' });
 
 export const signIn = (formData) => API.post('/users/signin', formData);
 export const signUp = (formData) => API.post('/users/signup', formData);
