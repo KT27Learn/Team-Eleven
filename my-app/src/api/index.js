d11f5987f6ac93a3df4b7c68e07e73276f60ffd8
KT27Learn <66994197+KT27Learn@users.noreply.github.com>
2021-07-20 19:46:03 +0800

    Milestone 3 Commit Bugfix v2.0

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://team-eleven-backend.herokuapp.com' });
 
 export const signIn = (formData) => API.post('/users/signin', formData);
 export const signUp = (formData) => API.post('/users/signup', formData);
@@ -17,3 +17,10 @@ export const fetchUserSession = (userDetails) => API.post('/session/past', userD
 export const fetchFavouritesLog = (userDetails) => API.post('/favourites/userlog', userDetails);
 export const favouriteMethod = (studyMethodDetails) => API.post('/favourites/favourite', studyMethodDetails);
 export const unfavouriteMethod = (studyMethodDetails) => API.post('/favourites/unfavourite', studyMethodDetails);
+export const fetchPosts = () => API.get('/posts/');
+export const addPost = (postDetails) => API.post('/posts/add', postDetails);
+export const sendFriendRequest = (usersdetails) => API.post('/users/addfriend', usersdetails);
+export const acceptFriendRequest = (usersdetails) => API.post('/users/acceptfriend', usersdetails);
+export const removeFriend = (usersdetails) => API.post('/users/removefriend', usersdetails);
+export const removeFriendRequest = (usersdetails) => API.post('/users/removerequest', usersdetails);
+export const updateFriends = (userid) => API.post('/users/updatefriends', userid);
